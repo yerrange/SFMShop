acf169038df67165f17a26087dbfa0035faba692
yerrange <stepanvasilyev00@gmail.com>
2026-06-04 13:07:28 +0300

    final task done

--- src/utils/calculations.py
+++ src/utils/calculations.py
@@ -1,7 +1,3 @@
-import time
-from datetime import datetime
-
-
 def calculate_discount(price, discount_rate):
     return price * discount_rate
 
@@ -12,18 +8,20 @@ def calculate_final_price(price, discount, delivery):
     return price - discount + delivery
 
 
+import time
+from datetime import datetime
+
+
 def measure_time(func):
     """Замеряет время работы функции"""
     def wrapper(*args, **kwargs):
         start = time.time()
-        result = func(*args, **kwargs)
+        func(*args, **kwargs)
         stop = time.time()
-        print(f'Функция отработала за {stop - start:.10f} секунд')
-        return result
+        return round(stop-start, 10)
     return wrapper
 
 
-@measure_time
 def find_product_in_list(products, product_id):
     """Поиск в списке (медленный)"""
     for product in products:
@@ -32,7 +30,6 @@ def find_product_in_list(products, product_id):
     return None
 
 
-@measure_time
 def find_product_in_dict(products, product_id):
     """Поиск в словаре (быстрый)"""
     return (
@@ -42,7 +39,7 @@ def find_product_in_dict(products, product_id):
     )
 
 
-def read_file_list(filename):
+def read_file_list_orders(filename):
     """Читает файл с заказами"""
     with open(filename, 'r', encoding='utf-8') as file:
         return [
@@ -55,7 +52,14 @@ def read_file_list(filename):
         ]
 
 
-def read_file_dict(filename):
+def read_file_list_products(filename) -> list:
+    """Читает файл с продуктами"""
+    with open(filename, 'r', encoding='utf-8') as file:
+        return [line.rstrip().split() for line in file.readlines()]
+
+
+def read_file_dict(filename) -> dict:
+    """Читает файл с продуктами"""
     with open(filename, 'r', encoding='utf-8') as file:
         return {
             int(line.rstrip().split()[0]): line.rstrip().split()[1:]
@@ -67,8 +71,8 @@ def read_file_dict(filename):
 # print(find_product_in_dict(read_file_dict("products_2000_shuffled.txt"), 55))
 
 
-@measure_time
 def sort_bubble(data, key_index=0):
+    """Медленная сортировка"""
     n = len(data)
 
     for i in range(n - 1):
@@ -85,13 +89,43 @@ def sort_bubble(data, key_index=0):
     return data
 
 
-@measure_time
 def sort_fast(data):
+    """Быстрая сортировка"""
     return sorted(data, key=lambda x: x[1])
 
 
-sort_bubble(read_file_list("orders_10000_unsorted.txt"))
-sort_fast(read_file_list("orders_10000_unsorted.txt"))
+# sort_bubble(read_file_list("orders_10000_unsorted.txt"))
+# sort_fast(read_file_list("orders_10000_unsorted.txt"))
+
+
+def compare_functions_speed(func_1, func_2):
+    """Сравнивает скорость работы функций"""
+    test_1 = measure_time(func_1[0])(*func_1[1])
+    test_2 = measure_time(func_2[0])(*func_2[1])
+    speedup = test_1 - test_2
+    return round(test_1 / test_2 if speedup > 0 else 0, 2)
+
+
+if __name__ == "__main__":
+    print(
+        "Оптимизированная функция сортировки отработала быстрее в",
+        compare_functions_speed(
+            (sort_bubble, (read_file_list_orders("orders_10000_unsorted.txt"), )),
+            (sort_fast, (read_file_list_orders("orders_10000_unsorted.txt"), ))
+        ),
+        "раз"
+    )
+
+    print(
+        "Оптимизированная функция поиска отработала быстрее в",
+        compare_functions_speed(
+            (find_product_in_list, (read_file_list_products("products_2000_shuffled.txt"), 55)),
+            (find_product_in_dict, (read_file_dict("products_2000_shuffled.txt"), 55))
+        ),
+        "раз"
+    )
+
+
 
 # Функция отработала за 3,4940419197 секунд
 # Функция отработала за 0,0025281906 секунд
@@ -100,3 +134,4 @@ sort_fast(read_file_list("orders_10000_unsorted.txt"))
 # на списке из 10000 записей,
 # т.к. сложность алгоритма пузырька n**2,
 # а sorted (Timsort) – n log n.
+
